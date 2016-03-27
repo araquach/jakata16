8e70bc95ae6bd9905605cf8cba719b798ff7d50d
Adam Carter <adam@jakatasalon.co.uk>
2016-03-27 10:47:15 +0000

    sorted a few bits

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,18 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
+Route::get('/details', 'PagesController@details');
+Route::get('/news', 'PagesController@news');
+Route::get('/recruitment', 'PagesController@recruitment');
+Route::get('/offers', 'PagesController@offers');
+Route::get('/prices', 'PagesController@prices');
+Route::get('/team', 'PagesController@team');
+
+
+Route::get('contact', 
+  ['as' => 'contact', 'uses' => 'ContactController@create']);
+Route::post('contact', 
+  ['as' => 'contact_store', 'uses' => 'ContactController@store']);
+  
+Route::resource('apprentice', 'ApprenticeController');
+Route::resource('stylist', 'StylistController');
