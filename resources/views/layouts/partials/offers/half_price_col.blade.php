86be53be99681300dacd0d0b2f2e920758340382
Adam Carter <adam@jakatasalon.co.uk>
2016-05-06 17:14:38 +0000

    Not sure?

---
+++ resources/views/layouts/partials/offers/half_price_col.blade.php
@@ -0,0 +1,17 @@
+<section id="offers">
+	
+	<section id="offer1">
+	  <h2>Half Price Colour <br>with every<br>Cut, Dry &amp; Style</h2>
+	  <p>For new clients<br>Until the end of September</p>
+	    <small><time datetime="2015-09-30">extended until 30/09/15</time><br>
+	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
+	</section>
+	
+	<section id="offer2">
+	  <h2><span class="big">30% off</span><br>Mens Cut &amp; Style<br>For New Clients</h2>
+	  <p>Throughout September</p>
+	    <small>until <time datetime="2015-09-30">extended until 30/09/15</time><br>
+	    Not with any other offer. Excludes Saturday<br></small>
+	</section>
+
+</section> <!--end #offers-->
