ad48068f318252a10f9deac47ac16bc0b775582b
Adam Carter <adam@jakatasalon.co.uk>
2016-06-25 13:22:44 +0000

    Created custom validation messages for contact form

--- app/Http/Requests/ContactFormRequest.php
+++ app/Http/Requests/ContactFormRequest.php
@@ -31,5 +31,19 @@ class ContactFormRequest extends Request {
 			'body' => 'required',
 		];
 	}
+	
+	public function messages()
+	{
+		return [
+    	    'first_name.required' => 'Please enter your first name',
+    	    'second_name.required' => 'Please enter your second name',
+    	    'email.required' => 'Please enter your email address',
+    	    'email.email' => 'Please enter a valid email address',
+    	    'email_confirm.required' => 'Please repeat your email address',
+    	    'email_confirm.same' => 'Please ensure you have entered your email address correctly',
+    	    'mobile.required' => 'Please enter your mobile number',
+    	    'body.required' => 'Your message cannot be blank',
+		];
+	}
 
 }
