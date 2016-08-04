98638b422e18e397cd12a68dd7498859d839e02d
Adam Carter <adam@jakatasalon.co.uk>
2016-08-04 12:25:04 +0000

    Changed date of offers

--- resources/views/pages/offers/half_price_col.blade.php
+++ resources/views/pages/offers/half_price_col.blade.php
@@ -4,15 +4,15 @@
 	
 	<section id="offer1">
 	  <h2>Half Price Colour <br>with every<br>Cut, Dry &amp; Style</h2>
-	  <p>For new clients<br>Until the end of July</p>
-	    <small><time datetime="2016-07-29">until 29/07/16</time><br>
+	  <p>For new clients<br>Until the end of August</p>
+	    <small><time datetime="2016-08-31">extended until 31/08/16</time><br>
 	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
 	</section>
 	
 	<section id="offer2">
 	  <h2><span class="big">30% off</span><br>Mens Cut &amp; Style<br>For New Clients</h2>
-	  <p>Throughout June/July</p>
-	    <small>until <time datetime="2016-07-29">29/07/16</time><br>
+	  <p>Throughout August</p>
+	    <small>extended until <time datetime="2016-08-31">31/08/16</time><br>
 	    Not with any other offer. Excludes Saturday<br></small>
 	</section>
 	
