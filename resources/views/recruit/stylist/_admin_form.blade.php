0c8ff4ecec186eb618259e07c0cf71149d1d5d49
Adam Carter <adam@jakatasalon.co.uk>
2016-08-27 10:00:28 +0000

    Fixed some of the recruit admin styling

--- resources/views/recruit/stylist/_admin_form.blade.php
+++ resources/views/recruit/stylist/_admin_form.blade.php
@@ -3,29 +3,31 @@
     'route' => ['stylist.update', $stylist->id]
 ]) !!}
 	
-	<div class="row">
-		{!! Form::label('quality', 'Rate the applicant') !!}
-		{!! Form::select('quality', array(
-			'1' => 'Discard',
-			'2' => 'Put on backburner',
-			'3' => 'Has Potential',
-			'5' => 'We need them',
-			), old('quality'), ['placeholder' => '-- Please Select --']) !!}
-		{!! $errors->first('quality', '<div class="errorMessage">:message</div>') !!}
-	</div>
-	
-	<div class="row">
-		{!! Form::label('contact_status', 'Contact Status') !!}
-		{!! Form::select('contact_status', array(
-			'1' => 'Not Contacted',
-			'2' => 'Left Message',
-			'3' => 'Spoke Direct',
-			), old('contact_status'), ['placeholder' => '-- Please Select --']) !!}
-		{!! $errors->first('contact_status', '<div class="errorMessage">:message</div>') !!}
-	</div>
-	
-	<div class="row buttons">
-	{!! Form::submit('Submit') !!}
+	<div class="form">
+		<div class="row">
+			{!! Form::label('quality', 'Rate the applicant') !!}
+			{!! Form::select('quality', array(
+				'1' => 'Discard',
+				'2' => 'Put on backburner',
+				'3' => 'Has Potential',
+				'5' => 'We need them',
+				), old('quality'), ['placeholder' => '-- Please Select --']) !!}
+			{!! $errors->first('quality', '<div class="errorMessage">:message</div>') !!}
+		</div>
+		
+		<div class="row">
+			{!! Form::label('contact_status', 'Contact Status') !!}
+			{!! Form::select('contact_status', array(
+				'1' => 'Not Contacted',
+				'2' => 'Left Message',
+				'3' => 'Spoke Direct',
+				), old('contact_status'), ['placeholder' => '-- Please Select --']) !!}
+			{!! $errors->first('contact_status', '<div class="errorMessage">:message</div>') !!}
+		</div>
+		
+		<div class="row buttons">
+		{!! Form::submit('Submit') !!}
+		</div>
 	</div>
 
 {!! Form::close() !!}
