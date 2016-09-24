efdc307b50666ab8dc8c097ee9beb427ad1fe655
Adam Carter <adam@jakatasalon.co.uk>
2016-09-24 06:51:32 +0000

    Filtered out discarded applicants

--- app/Http/Controllers/ApprenticeController.php
+++ app/Http/Controllers/ApprenticeController.php
@@ -30,7 +30,7 @@ class ApprenticeController extends Controller {
 	 */
 	public function index()
 	{
-		$apprentices = $this->apprentice->orderBy('quality')->get();
+		$apprentices = $this->apprentice->where('quality', '!=', '4')->orderBy('quality')->get();
 		
 		return view('recruit.apprentice.index', compact('apprentices'));
 	}
