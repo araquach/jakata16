86b99222c7b30c0033ebe5f8b374a71135390103
Adam Carter <adam@jakatasalon.co.uk>
2016-11-07 21:50:43 +0000

    Added new meta for offers

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -3,11 +3,11 @@
 @section('head')
 
 @include('layouts.partials.head', [
-	// 'description' => '£60 Colour & Cut Package with stylists or £75 package with a senior stylist. Plus 30% off men\'s cuts for new clients.',
+	'description' => '£60 Colour & Cut Package with stylists or £75 package with a senior stylist. Plus 30% off men\'s cuts for new clients.',
 	'keywords' => 'Jakata Offers, Jakata offers, hairdressing Offers, salon Offers, summer offers',
 	'ogtitle' => 'Offers',
-	// 'ogdescription' => '£60 Colour & Cut Package with stylists or £75 package with a senior stylist. Plus 30% off men\'s cuts for new clients. ',
-	// 'ogimage' => url('/') . '/images/offers/offers_0616_meta.jpg',
+	'ogdescription' => '£60 Colour & Cut Package with stylists or £75 package with a senior stylist. Plus 30% off men\'s cuts for new clients. ',
+	'ogimage' => url('/') . '/images/offers/offers_0616_meta.jpg',
 	'title' => 'Jakata Salon - Hairdressing Offers - Hairdressers in Warrington'
 	])
 	
