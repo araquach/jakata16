6885332371815bb5b19ed18920d50e6a062549b5
Adam Carter <adam@jakatasalon.co.uk>
2016-11-07 21:47:34 +0000

    Updated the new offers

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -3,10 +3,10 @@
 @section('head')
 
 @include('layouts.partials.head', [
-	// 'description' => 'Half price colour with every cut, dry and style for new clients this summer. Plus 30% off men\'s cuts for new clients.',
+	// 'description' => '£60 Colour & Cut Package with stylists or £75 package with a senior stylist. Plus 30% off men\'s cuts for new clients.',
 	'keywords' => 'Jakata Offers, Jakata offers, hairdressing Offers, salon Offers, summer offers',
 	'ogtitle' => 'Offers',
-	// 'ogdescription' => 'Half price colour with every cut, dry and style for new clients this summer. Plus 30% off men\'s cuts for new clients. ',
+	// 'ogdescription' => '£60 Colour & Cut Package with stylists or £75 package with a senior stylist. Plus 30% off men\'s cuts for new clients. ',
 	// 'ogimage' => url('/') . '/images/offers/offers_0616_meta.jpg',
 	'title' => 'Jakata Salon - Hairdressing Offers - Hairdressers in Warrington'
 	])
@@ -15,8 +15,8 @@
 
 @section('content')
 
-{{-- @include('layouts.partials.fb_like') --}}
+@include('layouts.partials.fb_like')
 
-@include('pages.offers.no_offer')
+@include('pages.offers.package')
 
 @stop
--- resources/views/pages/offers/package.blade.php
+++ resources/views/pages/offers/package.blade.php
@@ -3,21 +3,21 @@
 	<section id="offer1">
 	  <h2>Senior Stylist Colour &amp; Cut Package</h2>
 	  <p>for just &pound;75<br>with any of our Senior Stylists</p>
-	    <small><time datetime="2015-01-31">until 31/01/15</time><br>
+	    <small><time datetime="2016-11-30">until 30/11/16</time><br>
 	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
 	</section>
 	
 	<section id="offer2">
 	  <h2>Stylist Colour &amp; Cut Package</h2>
-	  <p>for just &pound;55<br>with any of our Stylists</p>
-	    <small><time datetime="2015-01-31">until 31/01/15</time><br>
+	  <p>for just &pound;60<br>with any of our Stylists</p>
+	    <small><time datetime="2016-11-30">until 30/11/16</time><br>
 	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
 	</section>
 	
-	<section id="offer3">
+	<section id="offer4">
 	  <h2>Men's Cut &amp; Style</h2>
 	  <p>for just &pound;14 with a Stylist<br>&pound;18 with a Senior Stylist</p>
-	    <small><time datetime="2015-01-31">until 31/01/15</time><br>
+	    <small><time datetime="2016-11-30">until 30/11/16</time><br>
 	    For New Clients - not with any other offer. Excludes Saturday</small>
 	</section>
 	
