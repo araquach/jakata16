1d5562d5539ed22af7a03ba0313cf1eb5d2e191e
Adam Carter <adam@jakatasalon.co.uk>
2016-12-06 11:30:13 +0000

    Took offers dowm

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -3,10 +3,10 @@
 @section('head')
 
 @include('layouts.partials.head', [
-	'description' => '£60 Colour & Cut Package with stylists or £75 package with a senior stylist. Plus men\'s Cut & Style from £14 for new clients.',
+	'description' => '',
 	'keywords' => 'Jakata Offers, Jakata offers, hairdressing Offers, salon Offers, summer offers',
 	'ogtitle' => 'Offers',
-	'ogdescription' => '£60 Colour & Cut Package with stylists or £75 package with a senior stylist. Plus men\'s Cut & Style from £14 for new clients. ',
+	'ogdescription' => '',
 	'ogimage' => url('/') . '/images/offers/offers_0616_meta.jpg',
 	'title' => 'Jakata Salon - Hairdressing Offers - Hairdressers in Warrington'
 	])
@@ -17,6 +17,6 @@
 
 @include('layouts.partials.fb_like')
 
-@include('pages.offers.package')
+@include('pages.offers.no_offer')
 
 @stop
