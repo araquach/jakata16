85268aa036048a7f786ed44dba4853089defe4bc
Adam Carter <adam@jakatasalon.co.uk>
2017-03-01 18:51:11 +0000

    Added role column to users

--- database/seeds/users_table_seeder.php
+++ database/seeds/users_table_seeder.php
@@ -15,6 +15,23 @@ class UsersTableSeeder extends Seeder
             'name' => 'Adam Carter',
             'email' => 'adam@jakatasalon.co.uk',
             'password' => Hash::make('blonde123'),
+            'role' => '1',
+            'remember_token' => null,
+        ]);
+        
+        DB::table('users')->insert([
+            'name' => 'Jimmy Sharpe',
+            'email' => 'jimmy@jakatasalon.co.uk',
+            'password' => Hash::make('jim123'),
+            'role' => '1',
+            'remember_token' => null,
+        ]);
+        
+        DB::table('users')->insert([
+            'name' => 'Isobelle Lamb',
+            'email' => 'izzy@jakatasalon.co.uk',
+            'password' => Hash::make('iz123'),
+            'role' => '2',
             'remember_token' => null,
         ]);
     }
