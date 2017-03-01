095e8206626c40bea60134448004b2c543da2ee0
Adam Carter <adam@jakatasalon.co.uk>
2017-03-01 19:16:09 +0000

    created the Superstylists table and model

--- app/User.php
+++ app/User.php
@@ -23,4 +23,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function superstylists()
+    {
+        return $this->hasMany('App\Superstylist');
+    }
 }
