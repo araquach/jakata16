8686b9231c116411262bcd3d2b05b8e7b45569e6
Adam Carter <adam@jakatasalon.co.uk>
2017-03-07 20:13:01 +0000

    added custom validation for 1 and 5

--- app/Http/CustomValidator.php
+++ app/Http/CustomValidator.php
@@ -10,7 +10,16 @@ class CustomValidator {
      
      public function validateOneOrFive($attribute, $value, $parameters, $validator)
      {
+        $data = collect($validator->getData());
          
+        $data = $data->slice(4);
+          
+        if($data->contains(1) or $data->contains(5))
+        {
+        return false;
+        }
+        
+        return true;
      }
 
 }
