d4fa18235fcff46a1e1ed45d547836bb948957cf
Adam Carter <adam@jakatasalon.co.uk>
2017-03-09 15:06:57 +0000

    turned off the 1 to 5 validator

---
+++ resources/views/superstylist/admin/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.main') 
+
+@section('head')
+
+@include('layouts.partials.head')
+	
+@stop
+
+@section('content')
+
+<div id="admin">
+    
+	<h1>Voters</h1>
+	
+	<p>These people still need to complete their votes:</p>
+	
+	<h2>Jakata</h2>
+	
+	
+	<h2>PK</h2>
+	
+
+</div>
+
+@stop
