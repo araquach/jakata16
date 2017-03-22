06d9d81544e5c64d389fa33bf10156cc8cd40e96
Adam Carter <adam@jakatasalon.co.uk>
2017-03-22 14:20:33 +0000

    created the ProspectOffer model with relations

--- app/Prospect.php
+++ app/Prospect.php
@@ -9,4 +9,9 @@ class Prospect extends Model
     protected $guarded = ['id'];
     
     protected $dates = ['created_at', 'updated_at'];
+    
+    public function offer()
+    {
+        return $this->hasMany('App\ProspectOffer');
+    }
 }
