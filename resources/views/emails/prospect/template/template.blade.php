39a759c7ef4f6b2ff0590e7216a089cad1d897ab
Adam Carter <adam@jakatasalon.co.uk>
2017-03-22 12:03:36 +0000

    Added Unique validation to prospectFormRequest

--- resources/views/emails/prospect/template/template.blade.php
+++ resources/views/emails/prospect/template/template.blade.php
@@ -244,7 +244,7 @@
             <!-- Email Footer : BEGIN -->
             <table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 680px;">
                 <tr>
-                    <td style="padding: 40px 10px;width: 100%;font-size: 12px; font-family: sans-serif; line-height:18px; text-align: center; color: #888888;" class="x-gmail-data-detectors">
+                    <td style="padding: 40px 10px;width: 100%;font-size: 12px; font-family: sans-serif; line-height:18px; text-align: center; color: #666666;" bgcolor="#222222" class="x-gmail-data-detectors">
                         <webversion style="color:#cccccc; text-decoration:underline; font-weight: bold;">View as a Web Page</webversion>
                         <br><br>
                         JakataSalon<br>90/92 Bridge Street, Warrington<br>01925 242960
