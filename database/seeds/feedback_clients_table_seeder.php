dacca2f017c5c6cd302cdc6098144366033e71c3
Adam Carter <adam@jakatasalon.co.uk>
2017-04-22 14:05:01 +0000

    Got laura hall and crumplin working

--- database/seeds/feedback_clients_table_seeder.php
+++ database/seeds/feedback_clients_table_seeder.php
@@ -16,7 +16,7 @@ class FeedbackClientsTableSeeder extends Seeder
             'stylist' => 'Maisie Thomson',
             'first_name' => 'Patricia',
             'last_name' => 'Carter',
-            'mobile' => '0798567373682',
+            'mobile' => '07921806884',
         ]);
         
         DB::table('feedback_clients')->insert([
@@ -24,7 +24,7 @@ class FeedbackClientsTableSeeder extends Seeder
             'stylist' => 'Jimmy Sharpe',
             'first_name' => 'Sarah',
             'last_name' => 'Reed',
-            'mobile' => '07985667545672',
+            'mobile' => '07921806884',
         ]);
         
         DB::table('feedback_clients')->insert([
@@ -32,7 +32,7 @@ class FeedbackClientsTableSeeder extends Seeder
             'stylist' => 'Vicky Rowland',
             'first_name' => 'Helen',
             'last_name' => 'Jones',
-            'mobile' => '079846764673682',
+            'mobile' => '07921806884',
         ]);
         
         DB::table('feedback_clients')->insert([
@@ -41,16 +41,16 @@ class FeedbackClientsTableSeeder extends Seeder
             'stylist' => 'Laura Hall',
             'first_name' => 'Ryan',
             'last_name' => 'Peters',
-            'mobile' => '0733337373682',
+            'mobile' => '07921806884',
         ]);
         
         DB::table('feedback_clients')->insert([
-            'id' => '1456',
-            'iris_id' => '4',
+            'id' => '1234',
+            'iris_id' => '1234',
             'stylist' => 'Laura Crumplin',
             'first_name' => 'Trevor',
             'last_name' => 'Smith',
-            'mobile' => '0733337373682',
+            'mobile' => '07921806884',
         ]);
     }
 }
