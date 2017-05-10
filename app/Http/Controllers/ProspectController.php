e839435e8d22e805b9759df3a62593886c5d5c19
Adam Carter <adam@jakatasalon.co.uk>
2017-05-10 18:25:30 +0000

    testing the email address works

--- app/Http/Controllers/ProspectController.php
+++ app/Http/Controllers/ProspectController.php
@@ -222,7 +222,7 @@ class ProspectController extends Controller
    		Mail::send('emails.prospect.taster.to_reception', $recipient, function($message) use ($recipient)
    		{
        		$message->from('booking@jakatasalon.co.uk', 'Jakata');
-			$message->to('adam@jakatasalon.co.uk');
+			$message->to('jimmy@jakatasalon.co.uk');
        		$message->subject('Taster Package Applicant');
    		});
         
