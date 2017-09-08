6ad9db969463b4704d25ae987347e55a3a7e24b3
Adam Carter <adam@jakatasalon.co.uk>
2017-09-08 15:08:00 +0000

    Added review seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         $this->call(OffersTableSeeder::class);
         $this->call(BlogsTableSeeder::class);
         $this->call(BlogParasTableSeeder::class);
+        $this->call(ReviewsTableSeeder::class);
     }
 }
--- database/seeds/offers_table_seeder.php
+++ database/seeds/offers_table_seeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 
-class OffersTableSeeder extends Seeder
+class ReviewsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -12,102 +12,20 @@ class OffersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('offers')->insert([
+        DB::table('reviews')->insert([
             'id' => '1',
-            'salon_id' => '1',
-            'client_id' => '1111',
-            'first_name' => 'Tracey',
-            'last_name' => 'Peterson',
-            'mobile' => '07921806884',
-            'email' => 'adam@jakatasalon.co.uk',
-            'gender' => 'f',
-            'stylist_level' => 'ss',
-            'opt_out' => '',
-            'created_at' => Carbon::now(),
+            'review' => 'Good service as always. Thanks Adam.',
+            'client' => 'Client: Anna Alexander',
+            'staff' => 'Staff: Adam Carter',
+            'salon' => '1'
         ]);
         
-        DB::table('offers')->insert([
+        DB::table('reviews')->insert([
             'id' => '2',
-            'salon_id' => '1',
-            'client_id' => '2222',
-            'first_name' => 'Helen',
-            'last_name' => 'jones',
-            'mobile' => '0777777777',
-            'email' => 'helen@jones.com',
-            'gender' => 'f',
-            'stylist_level' => 's',
-            'opt_out' => '',
-            'created_at' => Carbon::now(),
-        ]);
-        
-        DB::table('offers')->insert([
-            'id' => '3',
-            'salon_id' => '1',
-            'client_id' => '3333',
-            'first_name' => 'Sharon',
-            'last_name' => 'smith',
-            'mobile' => '0777777778',
-            'email' => 'sharon@smith.com',
-            'gender' => 'f',
-            'stylist_level' => 'g',
-            'opt_out' => '',
-            'created_at' => Carbon::now(),
-        ]);
-        
-        DB::table('offers')->insert([
-            'id' => '4',
-            'salon_id' => '1',
-            'client_id' => '4444',
-            'first_name' => 'Graham',
-            'last_name' => 'jones',
-            'mobile' => '0777777778',
-            'email' => 'sharon@smith.com',
-            'gender' => 'm',
-            'stylist_level' => 'ss',
-            'opt_out' => '',
-            'created_at' => Carbon::now(),
-        ]);
-        
-        DB::table('offers')->insert([
-            'id' => '5',
-            'salon_id' => '1',
-            'client_id' => '5555',
-            'first_name' => 'Mark Carter',
-            'last_name' => 'smith',
-            'mobile' => '0777777778',
-            'email' => 'sharon@smith.com',
-            'gender' => 'm',
-            'stylist_level' => 's',
-            'opt_out' => '',
-            'created_at' => Carbon::now(),
-        ]);
-        
-        DB::table('offers')->insert([
-            'id' => '6',
-            'salon_id' => '1',
-            'client_id' => '6666',
-            'first_name' => 'David',
-            'last_name' => 'jacobi',
-            'mobile' => '0777777778',
-            'email' => 'sharon@smith.com',
-            'gender' => 'm',
-            'stylist_level' => 'g',
-            'opt_out' => '',
-            'created_at' => Carbon::now(),
-        ]);
-        
-        DB::table('offers')->insert([
-            'id' => '7',
-            'salon_id' => '1',
-            'client_id' => '7777',
-            'first_name' => 'Alex',
-            'last_name' => 'androgeonous',
-            'mobile' => '0777777778',
-            'email' => 'sharon@smith.com',
-            'gender' => '',
-            'stylist_level' => 'ss',
-            'opt_out' => '',
-            'created_at' => Carbon::now(),
+            'review' => 'Good service as always. Thanks Jimmy.',
+            'client' => 'Client: Cruella Peterson',
+            'staff' => 'Staff: Adam Carter',
+            'salon' => '2'
         ]);
     }
 }
