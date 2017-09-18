5f3b4c00c1222b27d02f9af588913b4d4865cf37
Adam Carter <adam@jakatasalon.co.uk>
2017-09-18 16:13:53 +0000

    Getting the reviews filter done

--- resources/views/pages/reviews.blade.php
+++ resources/views/pages/reviews.blade.php
@@ -19,6 +19,18 @@
 	
 	<h1>Some of our recent client reviews</h1>
 	
+	<ul class="stylist-filter">
+		<li class="review-adam"><a href="{{ URL::to('reviews/adam') }}">Adam</a></li>
+		<li class="review-jimmy"><a href="{{ URL::to('reviews/jimmy') }}">Jimmy</a></li>
+		<li class="review-laurah"><a href="{{ URL::to('reviews/hall') }}">Laura H</a></li>
+		<li class="review-natalie"><a href="{{ URL::to('reviews/natalie') }}">Natalie</a></li>
+		<li class="review-maisie"><a href="{{ URL::to('reviews/maisie') }}">Maisie</a></li>
+		<li class="review-caleb"><a href="{{ URL::to('reviews/caleb') }}">Caleb</a></li>
+		<li class="review-lauram"><a href="{{ URL::to('reviews/minett') }}">Laura M</a></li>
+		<li class="review-vikki"><a href="{{ URL::to('reviews/vikki') }}">Vikki</a></li>
+		<li class="review-layla"><a href="{{ URL::to('reviews/layla') }}">Layla</a></li>
+	</ul>
+	
 	@foreach($reviews as $review)
 		<div class="review">
 			<p>"{{ $review->review }}"</p>
