835f1cb6379d858b39ca916cb83ba14d164f8441
Adam Carter <adam@jakatasalon.co.uk>
2017-10-08 15:11:10 +0000

    Turned off the vote reminder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,11 +28,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('voteStart')
-                  ->weekly()->tuesdays()->at('16:00');
+        // $schedule->command('voteStart')
+               //   ->weekly()->tuesdays()->at('16:00');
         
-        $schedule->command('voteReminder')
-                  ->weekly()->fridays()->at('13:00');
+        // $schedule->command('voteReminder')
+               //   ->weekly()->fridays()->at('13:00');
                  
         // $schedule->command('followUp1')
                 //  ->weekly()->wednesdays()->at('16:30');
